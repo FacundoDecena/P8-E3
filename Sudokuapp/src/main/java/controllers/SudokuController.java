c43d8385a119575ee91e55a81a3dfb73e9804403
Facundo Decena <facundo.decena@gmail.com>
2020-05-24 17:49:28 -0300

    Created functions

--- Sudokuapp/src/main/java/controllers/SudokuController.java
+++ Sudokuapp/src/main/java/controllers/SudokuController.java
@@ -7,6 +7,11 @@ import spark.Route;
 import spark.template.velocity.VelocityTemplateEngine;
 import util.Path;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 
 public class SudokuController {
@@ -15,4 +20,44 @@ public class SudokuController {
 
         return new VelocityTemplateEngine().render(new ModelAndView(model, Path.Template.LAYOUT));
     };
+
+    public static Route searchSolution = (Request req, Response res) ->{
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        //  int[] grid = req.queryParams("grid");
+        int[] grid = {
+                0,8,0,5,7,6,2,0,0,
+                0,0,0,4,0,2,0,0,0,
+                0,0,0,0,3,9,5,4,8,
+                6,3,0,9,0,0,8,5,2,
+                0,9,0,2,0,0,3,7,0,
+                8,0,0,0,5,0,6,9,4,
+                2,5,7,6,0,3,4,8,9,
+                3,0,8,7,0,0,0,2,5,
+                0,4,0,0,0,0,0,0,6,
+        };
+
+        return new VelocityTemplateEngine().render(new ModelAndView(model, Path.Template.LAYOUT));
+    };
+
+    private static void post(String uri, String img) throws Exception{
+        URL url = new URL(uri);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json; utf-8");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setDoOutput(true);
+        try(OutputStream os = connection.getOutputStream()){
+            byte[] input = img.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(connection.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+        }
+    }
 }
--- Sudokuapp/src/main/java/util/Path.java
+++ Sudokuapp/src/main/java/util/Path.java
@@ -4,8 +4,9 @@ import lombok.Getter;
 
 public class Path {
     public static class Web{
-        @Getter public static final String API = "/api/";
-        @Getter public static final String HOME = API + "home";
+        @Getter public static final String HOME = "home";
+        @Getter public static final String READ = "localhosthome";
+        @Getter public static final String UPDATE = "home";
     }
 
     public static class Template {
