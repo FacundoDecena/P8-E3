bb0a4844c1f545b340c5dbd25a8248956bf40d96
Facundo Decena <facundo.decena@gmail.com>
2020-06-02 20:48:30 -0300

    Changed this. to PizarraDAO.

--- Pizarra/src/main/java/dao/PizarraDAO.java
+++ Pizarra/src/main/java/dao/PizarraDAO.java
@@ -7,10 +7,10 @@ public class PizarraDAO {
     private static Pizarra pizarra = new Pizarra();
 
     public void update(Pizarra pizarra){
-        this.pizarra = pizarra;
+        PizarraDAO.pizarra = pizarra;
     }
 
     public Pizarra read(){
-        return this.pizarra;
+        return PizarraDAO.pizarra;
     }
 }
